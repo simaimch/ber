87e97cfe300d02cae8cb9cf603a3f5d3bc0c586c
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2019-10-05 14:22:20 +0200

    Random PC Name

--- scenes/dialog/pcName.gd
+++ scenes/dialog/pcName.gd
@@ -4,6 +4,11 @@ var fname = ""
 var lname = ""
 
 func _on_Confirm_pressed():
+	if fname.empty():
+		setFnameRandom()
+	if lname.empty():
+		setLnameRandom()
+	
 	GameManager.PlayerData.name.first = fname
 	GameManager.PlayerData.name.last = lname
 	GameManager.recalcUI()
@@ -22,3 +27,24 @@ func _on_LastName_text_changed(new_text):
 
 func _on_pcName_about_to_show():
 	pass
+
+func setFname(s):
+	fname = s
+	$CenterContainer/VBoxContainer/FirstNameContainer/FirstName.text = fname
+
+func setLname(s):
+	lname = s
+	$CenterContainer/VBoxContainer/LastNameContainer/LastName.text = lname
+
+func setFnameRandom():
+	setFname(GameManager.getValueFromList("names_first_female_90s"))
+	
+func setLnameRandom():
+	setLname(GameManager.getValueFromList("names_last"))
+
+func _on_FirstNameRandomButton_pressed():
+	setFnameRandom()
+
+
+func _on_LastNameRandomButton_pressed():
+	setLnameRandom()
