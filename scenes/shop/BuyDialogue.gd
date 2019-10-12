bd01b59d4d54c52f27cb14af499ea023fb971ce6
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2019-10-12 14:39:09 +0200

    Shop: Prevent buying of owned items

--- scenes/shop/BuyDialogue.gd
+++ scenes/shop/BuyDialogue.gd
@@ -7,14 +7,26 @@ func showBuyDialogue(i):
 	$CenterContainer/VBoxContainer/TextureRect.texture = load(item.texture)
 	$CenterContainer/VBoxContainer/PriceLabel.text = Util.formatMoney(item.price)
 	
-	if item.price > GameManager.PlayerData.money:
-		$CenterContainer/VBoxContainer/HBoxContainer/ButtonYes.disabled = true
-		$CenterContainer/VBoxContainer/PriceLabel.set("custom_colors/font_color",Color(1,0,0))
-	else:
-		$CenterContainer/VBoxContainer/HBoxContainer/ButtonYes.disabled = false
-		$CenterContainer/VBoxContainer/PriceLabel.set("custom_colors/font_color",Color(0,1,0))
-			
+	var buyButtonDisabled = false
 	
+	if item.price > GameManager.PlayerData.money:
+		#$CenterContainer/VBoxContainer/HBoxContainer/ButtonYes.disabled = true
+		$CenterContainer/VBoxContainer/PriceLabel.set("custom_colors/font_color",Color(1,0,0))
+		$CenterContainer/VBoxContainer/ErrorPriceTooHigh.show()
+		buyButtonDisabled = true
+	else:
+		#$CenterContainer/VBoxContainer/HBoxContainer/ButtonYes.disabled = false
+		$CenterContainer/VBoxContainer/PriceLabel.set("custom_colors/font_color",Color(0,1,0))
+		$CenterContainer/VBoxContainer/ErrorPriceTooHigh.hide()
+	
+	if GameManager.PlayerData.inventory.has(item.ID):
+		$CenterContainer/VBoxContainer/ErrorOwned.show()
+		buyButtonDisabled = true
+	else:
+		$CenterContainer/VBoxContainer/ErrorOwned.hide()
+	
+	$CenterContainer/VBoxContainer/HBoxContainer/ButtonYes.disabled = buyButtonDisabled
+		
 	popup_centered()
 
 func _on_ButtonYes_pressed():
